2f98a3c4050bb7f788cee0c8192ed9595198cb84
Nandinigolla <nnandinigolla@gmail.com>
2026-03-03 19:05:10 +0530

    Added gunicorn

--- crusto/settings.py
+++ crusto/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pu&1d4p519&*vskt3!8skc2$c2$e+o)v!xfp9kktl(%i1802*w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
