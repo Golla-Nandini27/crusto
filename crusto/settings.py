9a0742f1dc00513cfa3e200d677d107d12c11d98
Nandinigolla <nnandinigolla@gmail.com>
2026-03-03 20:36:20 +0530

    Production safe settings update

--- crusto/settings.py
+++ crusto/settings.py
@@ -11,12 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pu&1d4p519&*vskt3!8skc2$c2$e+o)v!xfp9kktl(%i1802*w'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["crusto.onrender.com"]
 
 
 # Application definition
