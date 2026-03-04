ae2844ba223b5d5207929a6685c5b20c831afdea
Nandinigolla <nnandinigolla@gmail.com>
2026-03-04 10:18:27 +0530

    remove superuser code from settings

--- crusto/settings.py
+++ crusto/settings.py
@@ -84,10 +84,10 @@ WSGI_APPLICATION = 'crusto.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600
+    )
 }
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
