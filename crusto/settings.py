578140db23a9ad5287fe23107fe639c9148f4ccc
Nandinigolla <nnandinigolla@gmail.com>
2026-03-04 09:49:12 +0530

    admin user

--- crusto/settings.py
+++ crusto/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'crusto.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
